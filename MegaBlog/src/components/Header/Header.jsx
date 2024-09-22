8d0e341c7913570b55bd62d2d850fa93ae75c458
Anu9969 <sharmaanurag9969@gmail.com>
2024-09-22 08:52:00 +0530

    building Header,NavItems added

--- MegaBlog/src/components/Header/Header.jsx
+++ MegaBlog/src/components/Header/Header.jsx
@@ -1,6 +1,45 @@
 import React from 'react'
+import {Container , Logo , LogoutBtn} from '../index'
+import {Link} from 'react-router-dom'
+import { useSelector } from 'react-redux' //tabhi to pta chalega ki user logged in h ki nhi
+import { useNavigate } from 'react-router-dom'
+
+
 
 function Header() {
+  //sabse pehle state m se nikaalna padega ki authenticated h ya nhi
+  const authStatus = useSelector((state) => state.auth.status) //authslice waale status ko access kr rhe h
+  
+  const navigate = useNavigate()  //is tarah ki nav bar jb bnti h to ek array bna ke usme loop kiya jaata h
+  const navItems =[
+    {
+      name: "Home",
+      slug: "/",
+      active: true
+    },
+    {
+      name:"Login",
+      slug: "/login",
+      active: !authStatus
+    },
+    {
+      name: "Signup",
+      slug: "/signup",
+      active: !authStatus
+    },
+    {
+      name: "All Posts",
+      slug: "/all-posts",
+      active:authStatus,
+
+    },
+    {
+      name: "Create Post",
+      slug: "/create-post",
+      active:authStatus
+    }
+  ]
+  
   return (
     <div>Header</div>
   )
--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -1,4 +1,9 @@
 import Header from './Header/Header'; // Corrected relative path for Header
 import Footer from './Footer/Footer'; // Corrected relative path for Footer (adjust if Footer is elsewhere)
+import Container from './container/Container';
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
 
-export { Header, Footer };
+
+export { Header, Footer, Container , Logo, LogoutBtn}; // Export Header, Footer, and Container components
+
